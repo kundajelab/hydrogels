046ae494a9166a1188c2bab96959c374a0a0474c
annashcherbina <annashch@stanford.edu>
2018-10-07 15:05:54 -0700

    generate counts files

---
+++ peak_aggregation_to_counts/extract_columns_for_comparisons.sh
@@ -0,0 +1,2 @@
+cut -f1,2,3,10,11,12,13,14,15,16,17 hydrogel.atac.counts.txt > 2D.soft.tcps.mg.txt
+cut -f1,2,3,4,5,6,7,8,9,18,19,20,21,22,23 hydrogel.atac.counts.txt > hydrogel.soft.stiff.sahah.txt
